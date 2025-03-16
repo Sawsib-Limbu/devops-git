58e315f76d5f376f0f75e5c935653d781f3b9ce8
abcd <abc@gmail.com>
2025-03-16 02:27:58 +0000

    add if condition to check apache status

--- install_apache.sh
+++ install_apache.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo"Installing apache2 pkg"
 sudo apt update
+sudo apt install apache2 -y
+if [ -f /var/run/apache2/apache2.pid]
+then
+     echo "apache2 is running"
+else
+	echo "apache seems not running,starting the service now..."
+sudo systemctl start apache2
